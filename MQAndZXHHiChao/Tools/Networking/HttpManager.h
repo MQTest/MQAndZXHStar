fa29e8c04d69d5636e79bd07872d2e2722045d7e
MQGreen <MaQi@maqi-3.local>
2016-07-04 15:36:38 +0800

    mq
    update  AFN

---
+++ MQAndZXHHiChao/Tools/Networking/HttpManager.h
@@ -0,0 +1,13 @@
+//
+//  HttpManager.h
+//  MQAndZXHHiChao
+//
+//  Created by MaQi on 16/7/4.
+//  Copyright © 2016年 MQ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HttpManager : NSObject
+
+@end
